aee7cfec3ac2dfb351e0016a5b2460c310476b43
MdOnim <MdOnim@users.noreply.github.com>
2026-07-08 00:46:15 +0600

    add user profile and admin routes

---
+++ routes/user.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {
+  getProfile,
+  updateProfile,
+  getAllUsers,
+  blockUser,
+  unblockUser,
+  makeVolunteer,
+  makeAdmin,
+} = require('../controllers/user.controller');
+const {
+  verifyJWT,
+  verifyAdmin,
+} = require('../middleware/verifyJWT');
+
+const router = express.Router();
+
+router.get('/profile', verifyJWT, getProfile);
+router.put('/profile', verifyJWT, updateProfile);
+router.get('/all', verifyJWT, verifyAdmin, getAllUsers);
+router.patch('/block/:id', verifyJWT, verifyAdmin, blockUser);
+router.patch('/unblock/:id', verifyJWT, verifyAdmin, unblockUser);
+router.patch('/make-volunteer/:id', verifyJWT, verifyAdmin, makeVolunteer);
+router.patch('/make-admin/:id', verifyJWT, verifyAdmin, makeAdmin);
+
+module.exports = router;
