f5a4fd7aa9a7ca742284dec39a3ad0bfe016f157
MdOnim <MdOnim@users.noreply.github.com>
2026-07-08 00:46:17 +0600

    add divisions data and location routes

---
+++ routes/location.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const districts = require('../data/districts');
+const upazilas = require('../data/upazilas');
+const divisions = require('../data/divisions');
+
+const router = express.Router();
+
+router.get('/divisions', (req, res) => {
+  res.send(divisions);
+});
+
+router.get('/districts', (req, res) => {
+  const { division } = req.query;
+  if (division) {
+    const filtered = districts.filter((d) => d.division_id === division);
+    return res.send(filtered);
+  }
+  res.send(districts);
+});
+
+router.get('/upazilas/:district', (req, res) => {
+  const districtUpazilas = upazilas.filter(
+    (u) => u.district === req.params.district
+  );
+  res.send(districtUpazilas);
+});
+
+module.exports = router;
